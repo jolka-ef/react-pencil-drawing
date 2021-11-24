e78af70c2719203bf6062142db1dee321b9015e0
Agata <a.firlejczyk@gmail.com>
2021-11-24 15:59:54 +0100

    Updates

---
+++ precache-manifest.24f68de4e92b9eef62824b31201a3ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4552f73e08fbc505f0b91d284ef44490",
+    "url": "/react-pencil-drawing/index.html"
+  },
+  {
+    "revision": "5526155858572d730b38",
+    "url": "/react-pencil-drawing/static/css/main.d64c3c21.chunk.css"
+  },
+  {
+    "revision": "7f64db4ee07366139bd1",
+    "url": "/react-pencil-drawing/static/js/2.8e5e124a.chunk.js"
+  },
+  {
+    "revision": "5526155858572d730b38",
+    "url": "/react-pencil-drawing/static/js/main.36584034.chunk.js"
+  },
+  {
+    "revision": "c3997f218abd33dae34d",
+    "url": "/react-pencil-drawing/static/js/runtime~main.5d991647.js"
+  }
+]);

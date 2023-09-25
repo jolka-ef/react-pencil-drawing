f1f8da78d92c67bfa4e09916797b5dc4dd4badbc
Jolka <firjola@gmail.com>
2023-09-25 22:32:19 +0200

    gh pages

---
+++ precache-manifest.fa6bc66fcfabb366514f93e6e2302bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "204d8f962c6f4d1609878dc9b42cf6fd",
+    "url": "/react-pencil-drawing/index.html"
+  },
+  {
+    "revision": "4e1d352891fa3d5ad5e4",
+    "url": "/react-pencil-drawing/static/css/main.d64c3c21.chunk.css"
+  },
+  {
+    "revision": "7f64db4ee07366139bd1",
+    "url": "/react-pencil-drawing/static/js/2.8e5e124a.chunk.js"
+  },
+  {
+    "revision": "4e1d352891fa3d5ad5e4",
+    "url": "/react-pencil-drawing/static/js/main.f44525d8.chunk.js"
+  },
+  {
+    "revision": "c3997f218abd33dae34d",
+    "url": "/react-pencil-drawing/static/js/runtime~main.5d991647.js"
+  }
+]);
